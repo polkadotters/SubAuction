f6d804476c488fa04609e11abc884f8a3822b62c
green-jay <greenee@email.cz>
2021-01-02 10:53:00 +0100

    Add burn and destroy to NFT

--- pallets/nft/src/lib.rs
+++ pallets/nft/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_event, decl_module, decl_storage, dispatch::{DispatchError, DispatchResult}};
+use frame_support::{decl_event, decl_module, decl_storage, dispatch::{DispatchResult}};
 use frame_system::ensure_signed;
 use sp_runtime::traits::StaticLookup;
 
@@ -23,9 +23,7 @@ decl_module! {
 		#[weight = 1000]
 		pub fn create_token_class(origin, metadata: CID, data: T::ClassData) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
-
-			let _result: Result<T::ClassId, DispatchError> = orml_nft::Module::<T>::create_class(&sender, metadata, data);
-
+			let _result = orml_nft::Module::<T>::create_class(&sender, metadata, data)?;
 			Self::deposit_event(RawEvent::NFTTokenClassCreated(sender));
 			Ok(())
 		}
@@ -34,19 +32,33 @@ decl_module! {
 		pub fn mint_tokens(origin, class_id: <T as orml_nft::Trait>::ClassId, metadata: CID, 
                            token_data: <T as orml_nft::Trait>::TokenData) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
-
-			let _result: Result<T::TokenId, DispatchError> = orml_nft::Module::<T>::mint(&sender, class_id, metadata, token_data);
-
-            Self::deposit_event(RawEvent::NFTTokenMinted(sender));
+			let _result = orml_nft::Module::<T>::mint(&sender, class_id, metadata, token_data)?;
+            Self::deposit_event(RawEvent::NFTTokenMinted(sender, class_id));
             Ok(())
 		}
 
 		#[weight = 1000]
-		pub fn transfer_token(origin, dest: <T::Lookup as StaticLookup>::Source, token_class_id: T::ClassId, token_id: T::TokenId) -> DispatchResult {
+		pub fn transfer_token(origin, dest: <T::Lookup as StaticLookup>::Source, token: (T::ClassId, T::TokenId)) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
 			let to: T::AccountId = T::Lookup::lookup(dest)?;
-			let _result = orml_nft::Module::<T>::transfer(&sender, &to, (token_class_id, token_id));
-			Self::deposit_event(RawEvent::NFTTokenTransferred(sender, to));
+			let _result = orml_nft::Module::<T>::transfer(&sender, &to, token);
+			Self::deposit_event(RawEvent::NFTTokenTransferred(sender, to, token.0, token.1));
+			Ok(())
+		}
+
+		#[weight = 1000]
+		pub fn burn_token(origin, token: (T::ClassId, T::TokenId)) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let _result = orml_nft::Module::<T>::burn(&sender, token)?;
+			Self::deposit_event(RawEvent::NFTTokenBurned(sender, token.1));
+			Ok(())
+		}
+
+		#[weight = 1000]
+		pub fn destroy_token_class(origin, class_id: T::ClassId) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let _result = orml_nft::Module::<T>::destroy_class(&sender, class_id)?;
+			Self::deposit_event(RawEvent::NFTTokenClassDestroyed(sender, class_id));
 			Ok(())
 		}
 
@@ -55,9 +67,13 @@ decl_module! {
 
 decl_event!(
 	pub enum Event<T> where
-	AccountId = <T as frame_system::Trait>::AccountId {
+	AccountId = <T as frame_system::Trait>::AccountId,
+	ClassId = <T as orml_nft::Trait>::ClassId,
+	TokenId = <T as orml_nft::Trait>::TokenId {
 		NFTTokenClassCreated(AccountId),
-		NFTTokenMinted(AccountId),
-		NFTTokenTransferred(AccountId, AccountId),
+		NFTTokenMinted(AccountId, ClassId),
+		NFTTokenTransferred(AccountId, AccountId, ClassId, TokenId),
+		NFTTokenBurned(AccountId, TokenId),
+		NFTTokenClassDestroyed(AccountId, ClassId),
 	}
 );
