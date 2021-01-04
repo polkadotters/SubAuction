46f175cd857949961e53694b960685863986a702
Petr Mensik <petr.mensik1@gmail.com>
2021-01-04 11:26:16 +0100

    Add unit tests skelet

--- pallets/nft/src/tests.rs
+++ pallets/nft/src/tests.rs
@@ -1,29 +1,41 @@
 use super::*;
 
-use crate::{Module, Trait, SubHandler};
 use sp_core::H256;
-use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
+use frame_support::{
+	impl_outer_origin, impl_outer_event, parameter_types, weights::Weight,
+	assert_ok, assert_noop,
+};
 use sp_runtime::{
 	traits::{BlakeTwo256, IdentityLookup}, testing::Header, Perbill,
 };
-use frame_system as system;
 
 impl_outer_origin! {
-	pub enum Origin for Test {}
+	pub enum Origin for Test where system = frame_system {}
 }
 
-// Configure a mock runtime to test the pallet.
+mod nfc {
+	// Re-export needed for `impl_outer_event!`.
+	pub use super::super::*;
+}
+
+
+impl_outer_event! {
+	pub enum Event for Test {
+		frame_system<T>,
+		nfc<T>,
+	}
+}
 
 #[derive(Clone, Eq, PartialEq)]
 pub struct Test;
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: Weight = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+    pub const MaximumBlockWeight: Weight = 1024;
+    pub const MaximumBlockLength: u32 = 2 * 1024;
+    pub const AvailableBlockRatio: Perbill = Perbill::one();
 }
 
-impl system::Trait for Test {
+impl frame_system::Trait for Test {
 	type BaseCallFilter = ();
 	type Origin = Origin;
 	type Call = ();
@@ -34,7 +46,7 @@ impl system::Trait for Test {
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = ();
+	type Event = Event;
 	type BlockHashCount = BlockHashCount;
 	type MaximumBlockWeight = MaximumBlockWeight;
 	type DbWeight = ();
@@ -52,15 +64,27 @@ impl system::Trait for Test {
 }
 
 impl Trait for Test {
-	type Event = ();
-	type Balance = u128;
-	type AuctionId = u64;
-	type Handler = SubHandler;
+	type Event = Event;
 }
 
-pub type TemplateModule = Module<Test>;
+type NfcModule = Module<Test>;
+type System = frame_system::Module<Test>;
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+	let mut t: sp_io::TestExternalities = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
+	t.execute_with(|| System::set_block_number(1) );
+	t
+}
+
+fn last_event() -> Event {
+	System::events().last().unwrap().event.clone()
+}
+
+#[test]
+fn can_create_token_class() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(NftModule::create_class(Origin::signed(1), "token", ()));
+		// how the hell query the orml_nft storage
+	})
 }
