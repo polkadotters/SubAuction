b145f8d3ab0e5cd0331a9144c2e1d874016015ef
green-jay <greenee@email.cz>
2021-01-24 13:45:19 +0100

    Add default toolchain

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,4 +8,5 @@ if [ -z $CI_PROJECT_NAME ] ; then
     rustup update nightly-2020-10-05
 fi
 
+rustup default nightly-2020-10-05-x86_64-unknown-linux-gnu
 rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-05
