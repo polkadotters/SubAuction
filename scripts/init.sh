8f30dfa2bed4298aa94d849c9fee9fb5e826740d
green-jay <greenee@email.cz>
2021-04-28 12:05:13 +0200

    Use new toolchain

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,8 @@ set -e
 echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
-    rustup update nightly-2020-10-05
+    rustup update nightly-2021-02-12
 fi
 
-rustup default nightly-2020-10-05-x86_64-unknown-linux-gnu
-rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-05
+rustup default nightly-2021-02-12-x86_64-unknown-linux-gnu
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-02-12
